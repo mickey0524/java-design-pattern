ef843210f5694d05ab4b5dde4ca96c8470641154
mickey0524 <buptbh@163.com>
2019-06-03 20:48:18 +0800

    feat(design pattern): 新增代理模式

---
+++ src/Proxy/Main.java
@@ -0,0 +1,74 @@
+package Proxy;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+interface Image {
+    void showImage();
+}
+
+class HighResolutionImage implements Image {
+
+    private URL imageURL;
+    private long startTime;
+    private int height;
+    private int width;
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public HighResolutionImage(URL imageURL) {
+        this.imageURL = imageURL;
+        this.startTime = System.currentTimeMillis();
+        this.width = 600;
+        this.height = 600;
+    }
+
+    public boolean isLoad() {
+        // 模拟图片加载，延迟 3s 加载完成
+        long endTime = System.currentTimeMillis();
+        return endTime - startTime > 3000;
+    }
+
+    @Override
+    public void showImage() {
+        System.out.println("Real Image: " + imageURL);
+    }
+}
+
+class ImageProxy implements Image {
+
+    private HighResolutionImage highResolutionImage;
+
+    public ImageProxy(HighResolutionImage highResolutionImage) {
+        this.highResolutionImage = highResolutionImage;
+    }
+
+    @Override
+    public void showImage() {
+        while (!highResolutionImage.isLoad()) {
+            try {
+                System.out.println("Temp Image: " + highResolutionImage.getWidth() + " " + highResolutionImage.getHeight());
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        highResolutionImage.showImage();
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws MalformedURLException {
+        String image = "http://image.jpg";
+        URL url = new URL(image);
+        HighResolutionImage highResolutionImage = new HighResolutionImage(url);
+        ImageProxy imageProxy = new ImageProxy(highResolutionImage);
+        imageProxy.showImage();
+    }
+}
